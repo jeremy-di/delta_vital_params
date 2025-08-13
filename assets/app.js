51ad2a566200c8e3b68fd5df6d7561efb00acf8a
jeremy-di <delta.i.developpement@gmail.com>
2025-08-13 14:41:59 +0200

    first

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+// import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -8,3 +8,15 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+console.log("coucou")
+
+console.log('app.js chargé par Encore');
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', (e) => {
+    if (e.target.closest('.basculer')) {
+      document.body.classList.toggle('open');
+      console.log('body classes ->', document.body.className);
+    }
+  });
+});
